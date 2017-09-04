38e948299db146c1c54bc50cd61ee1c066af6f29
muhammad_indra <muhammad.indra65@gmail.com>
2017-09-05 03:19:42 +0700

    Signed-off-by: muhammad_indra <muhammad.indra65@gmail.com>

--- locale/app/Http/Controllers/HomeController.php
+++ locale/app/Http/Controllers/HomeController.php
@@ -9,6 +9,8 @@ use GuzzleHttp\Client;
 
 class HomeController extends Controller
 {
+    private $api_key = 'key-51e07bea59eac5a177a3c8e81e478a77';
+
     /**
      * Create a new controller instance.
      *
@@ -40,12 +42,27 @@ class HomeController extends Controller
     public function send(Request $request)
     {
         $client = new Client();
+        $user   = Auth::user();
+        $subscribe = $user->mailist; 
         
-        if ($request->mailist == null ) {
-           return "uncheck";
+        //subscribe
+        if($request->mailist == 'subscribe' && $subscribe !=1) {
+
+            $user->mailist = 1;
+            $user->save();
+
+            $res = $client->get('https://api.mailgun.net/v3/lists/pages', [
+                    'auth' => [
+                        'api' , $this->api_key
+                    ]
+                ]);
+            $res->setDefaultOption('verify', false);
+
+            dd($res->getBody());
         }
-        else{
-            return "check";
+        // unsubscribe
+        if($request->mailist != 'subscribe' && $subscribe == 1){
+
         }
     }
 
